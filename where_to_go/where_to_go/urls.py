96e092e3763305604f8788eb149ba007662f785b
konflic <konflic@mail.ru>
2021-11-28 12:40:27 +0300

    Setup media files serving

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', views.index),
-    path('admin/', admin.site.urls),
-]
+    path("", views.index),
+    path("admin/", admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
