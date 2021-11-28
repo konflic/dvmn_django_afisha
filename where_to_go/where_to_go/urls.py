e032424db79f45d2ebba176a05ee64513b688cb9
konflic <konflic@mail.ru>
2021-11-28 10:49:56 +0300

    Add frontend

--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -1,7 +1,7 @@
 """where_to_go URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from where_to_go import views
+from . import views
 
 
 urlpatterns = [
-    path("", views.index),
-    path("admin/", admin.site.urls),
+    path('', views.index),
+    path('admin/', admin.site.urls),
 ]
