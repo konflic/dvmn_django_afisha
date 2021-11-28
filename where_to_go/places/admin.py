64ccad7bbc79d6affbb3789720c1cd06c4792e55
konflic <konflic@mail.ru>
2021-11-28 11:59:52 +0300

    Add Place, Coordinates, Images models

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Place, Coordinate, Images
 
 # Register your models here.
+admin.site.register(Place)
+admin.site.register(Coordinate)
+admin.site.register(Images)
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.fields import CommaSeparatedIntegerField
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(max_length=300)
+    description_short = models.TextField()
+    description_long = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Coordinate(models.Model):
+    long = models.FloatField()
+    lat = models.FloatField()
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.place.title
+
+
+class Images(models.Model):
+    image_name = models.TextField()
+    place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.place.title
